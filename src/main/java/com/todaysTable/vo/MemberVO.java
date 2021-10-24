295f23b3e8a3193c285bc4a5b510ee3dd60b1e7b
Y00J00 <syeunnn@gmail.com>
2021-10-24 11:02:16 +0900

    [init] MemberVO 생성 MemberVO.java

---
+++ src/main/java/com/todaysTable/vo/MemberVO.java
@@ -0,0 +1,25 @@
+package com.todaysTable.vo;
+
+import java.sql.Date;
+
+public class MemberVO {
+	private int member_no;
+	private String id;
+	private String password;
+	private String name;
+	private Date birthdate;
+	private String email;
+	private String tel;
+	private String address;
+	private String nickname;
+	private String profile_image_path;
+	private Date sign_up_date;
+	private String admin_id;
+	}
+
+
+
+
+
+
+

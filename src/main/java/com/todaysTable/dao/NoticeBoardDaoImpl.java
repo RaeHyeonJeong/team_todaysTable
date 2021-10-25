60225ce0fab7b26e6c33f688342daf3b86a5a0fe
RaeHyeonJeong <me1411111@gmail.com>
2021-10-25 16:18:13 +0900

    [update] boardMapper.xml, NoticeBoardDao.java, NoticeBoardDaoImpl.java

--- src/main/java/com/todaysTable/dao/NoticeBoardDaoImpl.java
+++ src/main/java/com/todaysTable/dao/NoticeBoardDaoImpl.java
@@ -1,14 +1,50 @@
 package com.todaysTable.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.todaysTable.vo.NoticeBoardVO;
+
 @Repository
-public class NoticeBoardDaoImpl  {
+public class NoticeBoardDaoImpl  implements NoticeBoardDao{
 	
 	@Autowired
-	SqlSessionTemplate st;
+	SqlSessionTemplate sqlSession;
+
+	@Override
+	public List<NoticeBoardVO> selectAllBoard() {
+		return sqlSession.selectList("boardMapper.selectAllNoticeBoard");
+	}
+
+	@Override
+	public NoticeBoardVO selectBoard(NoticeBoardVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertBoard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateBoard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteBoard() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
 
 	
 }

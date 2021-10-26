6e08e0da5ecf9dd7a65826ee72ec59ef288a9bc8
Y00J00 <syeunnn@gmail.com>
2021-10-26 20:22:04 +0900

    [init] AdminVo생성 - AdminVo.java

---
+++ src/main/java/com/todaysTable/vo/AdminVO.java
@@ -0,0 +1,16 @@
+package com.todaysTable.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class AdminVO {
+	private String admin_id;
+	private String admin_password;
+
+	
+
+}

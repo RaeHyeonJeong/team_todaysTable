a4c023ab6a2561efd1c5ede46426e93027e01806
Y00J00 <syeunnn@gmail.com>
2021-10-29 00:38:26 +0900

    [update] getLoginInfo 생성 / MemberService.java

--- src/main/java/com/todaysTable/service/MemberService.java
+++ src/main/java/com/todaysTable/service/MemberService.java
@@ -1,6 +1,10 @@
 package com.todaysTable.service;
 
+import java.util.HashMap;
+
 public interface MemberService {
 	
 	public boolean loginCheck(String id, String password) throws Exception;
+	
+	public HashMap<String, Object> getLoginInfo(String id) throws Exception;
 }

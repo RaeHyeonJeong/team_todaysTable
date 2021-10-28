e59dc49322f15f53351f495c1c26150da8204e09
Y00J00 <syeunnn@gmail.com>
2021-10-29 00:37:18 +0900

    [update] "loginMapper.getLoginInfo"의 dao생성 / MemberDaoIpl.java

--- src/main/java/com/todaysTable/dao/MemberDaoIpl.java
+++ src/main/java/com/todaysTable/dao/MemberDaoIpl.java
@@ -1,7 +1,7 @@
 package com.todaysTable.dao;
 
+import java.util.HashMap;
 import org.mybatis.spring.SqlSessionTemplate;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -13,8 +13,12 @@ public class MemberDaoIpl implements MemberDao {
 
 	@Override
 	public String loginCk(String id) {
-		return sqlSession.selectOne("loginMapper.loginCk", id);
+		return sqlSession.selectOne("loginMapper.loginCk", id);// 로그인 체크를 위한 dao. 입력한 id값과 입력한 id값을 통해 dbms에서 가져온
+																// password가 있다.
+	}
+
+	@Override
+	public HashMap<String, Object> getLoginInfo(String id) {
+		return sqlSession.selectOne("loginMapper.getLoginInfo", id);// 입력한 id로 dbms에서 해당 회원의 name,profile_img_path를 가져옴
 	}
 }
-
-

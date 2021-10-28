d94bc5e2f578d2b0375a39fb6f56c398708ea939
Y00J00 <syeunnn@gmail.com>
2021-10-29 00:34:58 +0900

    [update] getLoginInfo의 dao 추가 / MemberDao.java

--- src/main/java/com/todaysTable/dao/MemberDao.java
+++ src/main/java/com/todaysTable/dao/MemberDao.java
@@ -1,6 +1,10 @@
 package com.todaysTable.dao;
 
+import java.util.HashMap;
+
 public interface MemberDao {
 
 	public String loginCk(String id) throws Exception;
+	
+	public HashMap<String, Object> getLoginInfo(String id);
 }

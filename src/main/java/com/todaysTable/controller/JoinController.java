534b8e3687ec8ab96bee91017e5f72c875238aea
ChoiYoonYoung <duddbs7890@daum.net>
2021-10-28 18:37:28 +0900

    [UPDATE]servlet-context.xml-multipartResolver 추가 memberMapper.xml JoinController.java signup.jsp SignupMemberDao.java SignupMemberDaoImpl.java

--- src/main/java/com/todaysTable/controller/JoinController.java
+++ src/main/java/com/todaysTable/controller/JoinController.java
@@ -1,10 +1,15 @@
 package com.todaysTable.controller;
 
+import java.io.FileOutputStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.todaysTable.dao.SignupMemberDao;
 import com.todaysTable.service.SignUpService;
@@ -28,13 +33,35 @@ public class JoinController {
 	}
 
 	// 회원가입
-	@RequestMapping(value = "signupsubmit.do", method = RequestMethod.POST)
-	public String joinSubmit(MemberVO vo) throws Exception {
+	@PostMapping(value = "signupsubmit.do")
+	public String joinSubmit(MemberVO vo,
+			@RequestParam(value = "file", required = false) MultipartFile file) throws Exception {
 		// 생일 yy/mm/dd로 수정
 		String birthdate = vo.getBirthdate();
 		birthdate = birthdate.replace('-', '/');
 		birthdate = birthdate.substring(2, birthdate.length());
 		vo.setBirthdate(birthdate);
+		// 파일업로드
+		String location = "C:\\Users\\KONGYI\\git\\team_todaysTable\\src\\main\\webapp\\resources\\upload\\";
+		FileOutputStream fos = null;
+		System.out.println(file);
+		String fileName = file.getOriginalFilename();
+		if (fileName.length() > 0) {
+			try {
+				fos = new FileOutputStream(location.concat(fileName));
+				fos.write(file.getBytes());
+				vo.setProfile_image_path(fileName);
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (fos != null)
+						fos.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		
 		// 회원가입 정보 넣어줌
 		dao.memberJoin(vo);
 		System.out.println(vo);
@@ -63,5 +90,5 @@ public class JoinController {
 
 		return Nresult;
 	}
-	
+
 }

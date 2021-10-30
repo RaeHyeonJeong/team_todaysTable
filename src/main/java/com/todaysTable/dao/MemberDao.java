e16fe287d637b2fe98ed56ac6b493380d44daa57
Y00J00 <syeunnn@gmail.com>
2021-10-30 15:02:42 +0900

    [update] getPersonalInfo,updateProfile 추가 / MemberDao.java

--- src/main/java/com/todaysTable/dao/MemberDao.java
+++ src/main/java/com/todaysTable/dao/MemberDao.java
@@ -2,9 +2,16 @@ package com.todaysTable.dao;
 
 import java.util.HashMap;
 
+import com.todaysTable.vo.MemberVO;
+
 public interface MemberDao {
 
 	public String loginCk(String id) throws Exception;
 	
 	public HashMap<String, Object> getLoginInfo(String id);
+	
+	public MemberVO getPersonalInfo(String id);
+	
+	public void updateProfile(MemberVO vo) ;
+	
 }

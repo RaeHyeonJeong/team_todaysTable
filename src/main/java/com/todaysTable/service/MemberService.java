a9e592c460b8057754075259abc0ed4bbb2c5393
Y00J00 <syeunnn@gmail.com>
2021-10-31 01:13:22 +0900

    [update]myPageMove,profileMove 수정/MemberService.java

--- src/main/java/com/todaysTable/service/MemberService.java
+++ src/main/java/com/todaysTable/service/MemberService.java
@@ -10,7 +10,9 @@ public interface MemberService {
 	
 	public HashMap<String, Object> getLoginInfo(String id) throws Exception;
 	
-	public MemberVO getPersonalInfo(String id) throws Exception;
+	public MemberVO myPageMove(String id) throws Exception;
 	
 	public void updateProfile(MemberVO vo)  throws Exception;
+	
+	public MemberVO profileMove(String id) throws Exception;
 }

cf319954d4c55d77e51643e38ba44d999f23e9c2
Y00J00 <syeunnn@gmail.com>
2021-10-30 15:05:01 +0900

    [update] getPersonalInfo, updateProfile 추가 / MemberService.java

--- src/main/java/com/todaysTable/service/MemberService.java
+++ src/main/java/com/todaysTable/service/MemberService.java
@@ -2,9 +2,15 @@ package com.todaysTable.service;
 
 import java.util.HashMap;
 
+import com.todaysTable.vo.MemberVO;
+
 public interface MemberService {
 	
 	public boolean loginCheck(String id, String password) throws Exception;
 	
 	public HashMap<String, Object> getLoginInfo(String id) throws Exception;
+	
+	public MemberVO getPersonalInfo(String id) throws Exception;
+	
+	public void updateProfile(MemberVO vo)  throws Exception;
 }

17efe50b16614e7db1997e01593181179b5a01b3
Y00J00 <syeunnn@gmail.com>
2021-10-31 18:54:23 +0900

    [update]<회원정보 가져오기/페이지 이동> 컨트롤러와 서비스 정리/MemberService.java

--- src/main/java/com/todaysTable/service/MemberService.java
+++ src/main/java/com/todaysTable/service/MemberService.java
@@ -6,13 +6,13 @@ import com.todaysTable.vo.MemberVO;
 
 public interface MemberService {
 	
-	public boolean loginCheck(String id, String password) throws Exception;
+	public boolean loginCheck(String id, String password) throws Exception;//로그인
 	
-	public HashMap<String, Object> getLoginInfo(String id) throws Exception;
+	public HashMap<String, Object> getLoginInfo(String id) throws Exception;//로그인
 	
-	public MemberVO myPageMove(String id) throws Exception;
+	public void updateProfile(MemberVO vo)  throws Exception;//회원정보 업데이트
 	
-	public void updateProfile(MemberVO vo)  throws Exception;
+	public MemberVO getPersonalInfo(String id) throws Exception;//회원정보 가져오기
 	
-	public MemberVO profileMove(String id) throws Exception;
+	
 }

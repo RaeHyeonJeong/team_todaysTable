2f3282188ab3fdd901339f9b232ae8077c32c3db
Y00J00 <syeunnn@gmail.com>
2021-11-02 01:55:32 +0900

    [update]로그 아웃 기능 추가/MemberService.java

--- src/main/java/com/todaysTable/service/MemberService.java
+++ src/main/java/com/todaysTable/service/MemberService.java
@@ -2,17 +2,23 @@ package com.todaysTable.service;
 
 import java.util.HashMap;
 
+import javax.servlet.http.HttpSession;
+
 import com.todaysTable.vo.MemberVO;
 
 public interface MemberService {
 	
 	public boolean loginCheck(String id, String password) throws Exception;//로그인
 	
-	public HashMap<String, Object> getLoginInfo(String id) throws Exception;//로그인
+	public HashMap<String, Object> getLoginInfo(String id) throws Exception;//로그인 회원의 name, profile_img_path를 리턴
+	
+	public void logout(HttpSession session) throws Exception;//로그아웃
 	
 	public void updateProfile(MemberVO vo)  throws Exception;//회원정보 업데이트
 	
 	public MemberVO getPersonalInfo(String id) throws Exception;//회원정보 가져오기
+	
+	
 	
 	
 }

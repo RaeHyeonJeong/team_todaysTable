600fa7df507bfc24632d91c9d1532634e96b5b13
Y00J00 <syeunnn@gmail.com>
2021-11-02 01:56:10 +0900

    [update]로그 아웃 기능 추가/MemberDao.java

--- src/main/java/com/todaysTable/dao/MemberDao.java
+++ src/main/java/com/todaysTable/dao/MemberDao.java
@@ -2,6 +2,8 @@ package com.todaysTable.dao;
 
 import java.util.HashMap;
 
+import javax.servlet.http.HttpSession;
+
 import com.todaysTable.vo.MemberVO;
 
 public interface MemberDao {
@@ -13,5 +15,7 @@ public interface MemberDao {
 	public MemberVO getPersonalInfo(String id);
 	
 	public void updateProfile(MemberVO vo) ;
+	
+	public void logout(HttpSession session);
 	
 }

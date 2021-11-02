064a12e25f72fda1308caca0d2dbae17182abcb9
Y00J00 <syeunnn@gmail.com>
2021-11-02 18:52:14 +0900

    [update]delete기능 추가

--- src/main/java/com/todaysTable/service/MyListService.java
+++ src/main/java/com/todaysTable/service/MyListService.java
@@ -12,7 +12,9 @@ public interface MyListService {
 	public int getMemberNo(String id) throws Exception;
 	public StoreVO getStoreInfo(int store_no) throws Exception;
 	public Double getAvg_Grade(int store_no) throws Exception;
-	public void insertLikeInfo(DibsVO vo) throws Exception;
 	public DibsVO getLikeInfo(String id, int store_no) throws Exception;
+	public void insertLikeInfo(DibsVO vo) throws Exception;
+	public void deleteLikeInfo(DibsVO vo) throws Exception;
+	public int checkLike(DibsVO vo) throws Exception;
 	
 }

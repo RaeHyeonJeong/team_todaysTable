d973eae0f5bb5bf4620825eeaabc262ae2b39d00
Y00J00 <syeunnn@gmail.com>
2021-11-02 18:27:07 +0900

    [init]myListServiceIpl생성/myListServiceIpl.java

---
+++ src/main/java/com/todaysTable/service/MyListServiceIpl.java
@@ -0,0 +1,58 @@
+package com.todaysTable.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.todaysTable.dao.MyListDao;
+import com.todaysTable.vo.DibsVO;
+import com.todaysTable.vo.StoreVO;
+
+@Service
+public class MyListServiceIpl implements MyListService{
+	
+	@Autowired
+	private MyListDao MyListDao;
+
+	@Override
+	public int getMemberNo(String id) throws Exception {
+		return MyListDao.getMemberNo(id);
+	}
+
+	@Override
+	public StoreVO getStoreInfo(int store_no) throws Exception {
+		return MyListDao.getStoreInfo(store_no);
+	}
+
+	@Override
+	public Double getAvg_Grade(int store_no) throws Exception {
+		return MyListDao.getAvg_Grade(store_no);
+	}
+	
+	
+	@Override /*버전1*/
+	public DibsVO getLikeInfo(String id, int store_no) throws Exception{
+		DibsVO vo=new DibsVO();
+		StoreVO storeVO = MyListDao.getStoreInfo(store_no);
+		vo.setAddress(storeVO.getAddress());
+		vo.setStore_no(storeVO.getStore_no());
+		vo.setStore_name(storeVO.getName());
+		vo.setMemb_no(MyListDao.getMemberNo("id"));
+		vo.setAvg_grade(MyListDao.getAvg_Grade(store_no));
+		
+		return vo;
+	
+	}
+	
+	
+	@Override
+	public void insertLikeInfo(DibsVO vo) throws Exception {
+		this.MyListDao.insertLikeInfo(vo);
+	}
+	
+	
+
+	
+	
+	
+}

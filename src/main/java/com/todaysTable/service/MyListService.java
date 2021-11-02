569f185d088b4d0ae0858c37d88175583a9b94bf
Y00J00 <syeunnn@gmail.com>
2021-11-02 18:26:43 +0900

    [init]MyListService생성/MyListService.java

---
+++ src/main/java/com/todaysTable/service/MyListService.java
@@ -0,0 +1,18 @@
+package com.todaysTable.service;
+
+
+
+import com.todaysTable.vo.DibsVO;
+import com.todaysTable.vo.StoreVO;
+
+
+public interface MyListService {
+	
+	
+	public int getMemberNo(String id) throws Exception;
+	public StoreVO getStoreInfo(int store_no) throws Exception;
+	public Double getAvg_Grade(int store_no) throws Exception;
+	public void insertLikeInfo(DibsVO vo) throws Exception;
+	public DibsVO getLikeInfo(String id, int store_no) throws Exception;
+	
+}

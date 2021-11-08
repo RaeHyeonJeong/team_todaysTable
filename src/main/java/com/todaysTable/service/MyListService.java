789dbdb99a75cd361c3f536b7a6666dfc54107fe
Y00J00 <syeunnn@gmail.com>
2021-11-08 18:24:50 +0900

    [update]찜하기 관련 수정/MyListServiceIpl.java

--- src/main/java/com/todaysTable/service/MyListService.java
+++ src/main/java/com/todaysTable/service/MyListService.java
@@ -2,6 +2,8 @@ package com.todaysTable.service;
 
 
 
+import java.util.List;
+
 import com.todaysTable.vo.DibsVO;
 import com.todaysTable.vo.StoreVO;
 
@@ -12,9 +14,11 @@ public interface MyListService {
 	public int getMemberNo(String id) throws Exception;
 	public StoreVO getStoreInfo(int store_no) throws Exception;
 	public Double getAvg_Grade(int store_no) throws Exception;
+	
 	public DibsVO getLikeInfo(String id, int store_no) throws Exception;
 	public void insertLikeInfo(DibsVO vo) throws Exception;
 	public void deleteLikeInfo(DibsVO vo) throws Exception;
-	public int checkLike(DibsVO vo) throws Exception;
+	public List<DibsVO> getLikeList(int memb_no) throws Exception;
+	
 	
 }
